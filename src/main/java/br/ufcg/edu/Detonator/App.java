839d472766984822d65b8b448f9aa0737a14fad2
Gervasio Junior <juniorb08@gmail.com>
2016-05-05 11:50:02 +0000

    Initial commit

---
+++ src/main/java/br/ufcg/edu/Detonator/App.java
@@ -0,0 +1,13 @@
+package br.ufcg.edu.Detonator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
